78ca280db215063e41ef3683dd1d4cbc9e504bcb
Pavel Averin <averinjr@gmail.com>
2025-04-17 22:41:32 +0300

    Fix in init imports

--- bnm/__init__.py
+++ bnm/__init__.py
@@ -8,5 +8,5 @@ Author: Pavel Averin
 
 from .core import BNMetrics
 from .utils import generate_random_dag
-from .utils import dag_to_cpdag
-from .viz import compare_models_descriptive, compare_models_comparative, analyse_mb, generate_synthetic_data_from_dag
+from .utils import dag_to_cpdag, generate_synthetic_data_from_dag
+from .viz import compare_models_descriptive, compare_models_comparative, analyse_mb
